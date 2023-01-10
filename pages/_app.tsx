46df0892614c9dca239b4b1970ef1a1db4f27672
PavloMylkus <ppavlo82@gmail.com>
2023-01-10 13:08:45 +0200

    google verification

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,7 @@ const App = ({ Component, pageProps }: AppProps) => {
 				<meta name="keywords" content="погода на тиждень, погода сьогодні, погода завтра, погода на 3 дні, погода на вихідні, погода, погода Львів, прогноз погоди, прогноз на 5 днів, weather, forecast, myweather, myforecast" />
 				<meta name="author" content="Mylkus Pavlo" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<meta name="google-site-verification" content="5ACN9xiVpBOdb-YCxkg98SNLp1wvLFqogqYv3Mw6DIs" />
 			</Head>
 			<main>
 				<Component {...pageProps} />
